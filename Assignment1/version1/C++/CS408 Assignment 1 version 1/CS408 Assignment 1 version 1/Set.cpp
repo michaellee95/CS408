9bd541608a00072fe58b174fb650a8e7107d07d5
Byung Hun Lee <byunghunlee@cpp.edu>
2018-02-12 22:59:20 -0800

    c++

---
+++ Assignment1/version1/C++/CS408 Assignment 1 version 1/CS408 Assignment 1 version 1/Set.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Set.h"
+#include <string>
+
+using namespace std;
+
+Set::Set() {
+	cout << "Test2" << endl;
+}
+
+Set::Set(string input) {
+	cout << "Test" << endl;
+}
+
+void printSet() {
+
+}
---
+++ Assignment1/version1/C++/CS408 Assignment 1 version 1/CS408 Assignment 1 version 1/Set.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Set {
+public:
+	Set();
+	Set(string);
+	void printSet();
+};
+
+#endif 
