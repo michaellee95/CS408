e7c95629e85e560d05d4777999fc02c037fc2021
Byung Hun Lee <byunghunlee@cpp.edu>
2018-02-21 17:01:53 -0800

    FinalProject

---
+++ FinalProject/SQLJava.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class insert{
+	
+	public static void main (String args[]){
+		Class.forName(""oracle.jdbc.driver.OracleDriver"");
+		Connection con = DriverManager.getConnection( "jdbc:oracle:thin:@localhost:1521:orcl", "login1", "pwd1");
+		Statement stmt = con.createStatement();
+             
+		String q1 = "insert into userid values('" +id+ "', '" +pwd+ 
+							  "', '" +fullname+ "', '" +email+ "')";
+		stmt.executeUpdate(q1);
+	}
+}
